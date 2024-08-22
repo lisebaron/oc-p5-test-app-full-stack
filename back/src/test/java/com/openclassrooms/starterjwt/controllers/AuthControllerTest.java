ddb64d6b30a8522b7eace2344937b1b96b817957
LiseB <noodle@mbp-de-noodle.home>
2024-08-22 18:02:04 +0200

    testing AuthController

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
@@ -1,7 +1,101 @@
 package com.openclassrooms.starterjwt.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.payload.request.LoginRequest;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
+import com.openclassrooms.starterjwt.security.services.UserDetailsImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
 class AuthControllerTest {
 
+    private MockMvc mockMvc;
+
+    private JsonMapper mapper = new JsonMapper();
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private JwtUtils jwtUtils;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private Authentication authentication;
+
+    @Mock
+    private UserDetailsImpl userDetails;
+
+    @Mock
+    private User user;
+
+    private AuthController controller;
+
+    @BeforeEach
+    public void setUp(){
+        controller = new AuthController(authenticationManager, passwordEncoder, jwtUtils, userRepository);
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
+
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(authenticationManager.authenticate(any())).thenReturn(authentication);
+        when(jwtUtils.generateJwtToken(eq(authentication))).thenReturn("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9");
+    }
+
+    @Test
+    void authenticateUser_shouldAuthenticateUser_whenUserFound() throws Exception {
+        //Arrange
+        final var loginRequest = new LoginRequest();
+        loginRequest.setEmail("john.doe@test.com");
+        loginRequest.setPassword("password");
+
+        when(userDetails.getId()).thenReturn(1L);
+        when(userDetails.getUsername()).thenReturn("john.doe@test.com");
+        when(userDetails.getFirstName()).thenReturn("John");
+        when(userDetails.getLastName()).thenReturn("Doe");
+        when(userRepository.findByEmail(eq("john.doe@test.com"))).thenReturn(Optional.of(user));
+        when(user.isAdmin()).thenReturn(Boolean.FALSE);
+
+        //Act
+        final var result = mockMvc.perform(post("/api/auth/login")
+                .content(mapper.writeValueAsBytes(loginRequest))
+                .contentType(MediaType.APPLICATION_JSON_VALUE));
+
+        //Assert
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.token").value("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9"));
+        result.andExpect(jsonPath("$.id").value("1"));
+        result.andExpect(jsonPath("$.firstName").value("John"));
+        result.andExpect(jsonPath("$.lastName").value("Doe"));
+        result.andExpect(jsonPath("$.username").value("john.doe@test.com"));
+        result.andExpect(jsonPath("$.admin").value(Boolean.FALSE));
+    }
 }
--- back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerTest.java
@@ -2,40 +2,48 @@ package com.openclassrooms.starterjwt.controllers;
 
 import com.openclassrooms.starterjwt.mapper.SessionMapper;
 import com.openclassrooms.starterjwt.models.Session;
+import com.openclassrooms.starterjwt.security.services.UserDetailsServiceImpl;
 import com.openclassrooms.starterjwt.services.SessionService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = SessionController.class)
 class SessionControllerTest {
 
-    @Mock
-    private SessionService sessionService;
-
-    @Mock
-    private SessionMapper sessionMapper;
-
-    private SessionController sessionController;
-
-    @BeforeEach
-    void setUp() {
-        this.sessionController = new SessionController(sessionService, sessionMapper);
-    }
-
-    @Test
-    void findById_shouldFindSessionById() {
-        Session session = new Session();
-        when(sessionService.getById(7L)).thenReturn(session);
-
-        sessionController.findById(String.valueOf(7));
-
-
-    }
+    //wip
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    private SessionService sessionService;
+//
+//    @MockBean
+//    private SessionMapper sessionMapper;
+//
+//    @MockBean
+//    private UserDetailsServiceImpl userDetailsService;
+//
+//    @Mock
+//    private Session session;
+//
+//    @Test
+//    void findById_shouldFindSessionById() throws Exception {
+//        when(sessionService.getById(7L)).thenReturn(session);
+//
+//        mockMvc.perform(MockMvcRequestBuilders.get(
+//                "/api/session/7"
+//        )).andExpect(MockMvcResultMatchers.status().isOk());
+//    }
 
 }
