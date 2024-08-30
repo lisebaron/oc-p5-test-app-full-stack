56918c6b2114e140dbca8d4f93a4c3aa4e27aaab
LiseB <noodle@mbp-de-noodle.home>
2024-08-30 10:07:09 +0200

    reached coverage and finished unit tests

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -11,15 +11,36 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { LoginRequest } from '../../interfaces/loginRequest.interface';
+import { of, throwError } from 'rxjs';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  const mockRouter = {
+    navigate: jest.fn(),
+  };
+
+  const mockAuthService = {
+    login: jest.fn(),
+  }
+
+  const mockSessionService = {
+    logIn: jest.fn(),
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [
+        {provide: AuthService, useValue: mockAuthService},
+        {provide: Router, useValue: mockRouter},
+        {provide: SessionService, useValue: mockSessionService},
+      ],
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
@@ -36,7 +57,53 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should submit', () => {
+    //Arrange
+    const loginReq: LoginRequest = {
+      email: "john.doe@test.com",
+      password: "password"
+    }
+    component.form.setValue(loginReq);
+    const sessionInfo: SessionInformation = {
+      token: 'mockToken',
+      type: 'Bearer',
+      id: 1,
+      username: 'testuser',
+      firstName: 'John',
+      lastName: 'Doe',
+      admin: true
+    };
+    mockAuthService.login.mockReturnValue(of(sessionInfo));
+    
+    //Act
+    component.submit();
+
+    //Assert
+    expect(mockAuthService.login).toHaveBeenCalledWith(loginReq);
+    expect(mockSessionService.logIn).toHaveBeenCalledWith(sessionInfo);
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/sessions']);
+    expect(component.onError).toBe(false);
+  });
+
+  it("should not submit and set onError to true on login failure", () => {
+    //Arrange
+    mockAuthService.login.mockReturnValue(throwError(() => new Error('Login failed')));
+
+    //Act
+    component.submit();
+
+    //Assert
+    expect(component.onError).toBe(true);
+    expect(mockAuthService.login).toHaveBeenCalledTimes(1);
+    expect(mockSessionService.logIn).not.toHaveBeenCalledTimes(1);
+    expect(mockRouter.navigate).not.toHaveBeenCalledTimes(1);
   });
 });
--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -1,22 +1,68 @@
-import { HttpClientModule } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
-import { expect } from '@jest/globals';
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { expect } from "@jest/globals";
 
-import { TeacherService } from './teacher.service';
+import { TeacherService } from "./teacher.service";
+import { Teacher } from "../interfaces/teacher.interface";
 
-describe('TeacherService', () => {
+describe("TeacherService", () => {
   let service: TeacherService;
+
+  let httpTestingController: HttpTestingController;
+
+  const mockTeachers: Teacher[] = [
+    {id: 1, lastName: "Doe", firstName: "John", createdAt: new Date("2024-01-01T00:00:00Z"), updatedAt: new Date("2024-01-01T00:00:00Z")},
+    {id: 2, lastName: "Smith", firstName: "Jane", createdAt: new Date("2024-05-01T00:00:00Z"), updatedAt: new Date("2024-05-01T00:00:00Z")}
+  ];
+  
+  const mockTeacher: Teacher = {
+    id: 1, lastName: "Doe", firstName: "John", createdAt: new Date("2024-01-01T00:00:00Z"), updatedAt: new Date("2024-01-01T00:00:00Z")
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports:[
-        HttpClientModule
+        HttpClientTestingModule
       ]
     });
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(TeacherService);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should fetch all teachers", () => {
+    //Act
+    service.all().subscribe((teachers) => {
+      //Assert
+      expect(teachers).toEqual(mockTeachers);
+    });
+    
+    //Assert
+    const req = httpTestingController.expectOne("api/teacher");
+    expect(req.request.method).toBe("GET");
+    //Act
+    req.flush(mockTeachers);
+  });
+
+  it("should fetch a teacher by its Id", () => {
+    let teacherId = "1";
+    //Act
+    service.detail(teacherId).subscribe((teacher) => {
+      //Assert
+      expect(teacher).toEqual(mockTeacher);
+    });
+    
+    //Assert
+    const req = httpTestingController.expectOne(`api/teacher/${teacherId}`);
+    expect(req.request.method).toBe("GET");
+    //Act
+    req.flush(mockTeacher);
   });
 });
